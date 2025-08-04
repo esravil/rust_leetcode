25c8a666e7292220e9374e3cbaea8eda4df701a4
esravil <esravil99@gmail.com>
2025-08-04 14:49:40 -0400

    insert end of vec

--- problems/arrays/static_arrays/insert_end/src/main.rs
+++ problems/arrays/static_arrays/insert_end/src/main.rs
@@ -1,17 +1,19 @@
-// for this example, we are guaranteed the insertion is at the end
-
-fn insert_end(arr: &mut [i32], length: usize, n: i32) {
-
-    arr[length] = n;
-
-} // for arrays, if mutable, the arr type inside the sq brackets, and mutable keyword outside
-
 fn main() {
 
-    let mut arr: [i32; 5] = [1, 2, 3, 4, 5];
-    let length = arr.len() - 1; // or else this returns 5, 5 is out of bounds
+    let mut v: Vec<i32> = Vec::with_capacity(5);
+    insert_end(&mut v, 100);
+    println!("{:?}", &v[..]);
 
-    insert_end(&mut arr, length, 6); // dont forget in mut ref, need &mut before var
-    println!("{:?}", arr);
+}
+
+fn insert_end(arr: &mut Vec<i32>, val: i32) {
+
+    // first check if capacity of arr if full (in rust, arrs are known size so your length = capacity, but vectors are diff since unknown size
+    if arr.len() < arr.capacity() {
+
+        arr.push(val);
+
+    }
+
 
 }
