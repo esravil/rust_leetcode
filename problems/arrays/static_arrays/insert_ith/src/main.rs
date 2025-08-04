066914c5a0d2f9a1efc2a85747063636e3e8ff7b
esravil <esravil99@gmail.com>
2025-08-04 15:21:18 -0400

    revisited access

--- problems/arrays/static_arrays/insert_ith/src/main.rs
+++ problems/arrays/static_arrays/insert_ith/src/main.rs
@@ -1,29 +1,26 @@
-fn main() {
-    
-    let mut arr: [i32; 5] = [1, 2, 3, 4, 0]; // lets make it so that the 4 is in index 4
-    let l = arr.len();
+// for this problem, we will be inserting a val, x, at index i
+// can / should we assume arr is empty? n/a
 
-    insert_ith(&mut arr, 3, l, 5);
+fn main() {
+
+    let mut arr = [1, 2, 3, 4, 0]; // 0 indicates empty space..
+    insert_ith(&mut arr, 3, 99);
     println!("{:?}", arr);
 
 }
 
-fn insert_ith(arr: &mut [i32], i: usize, length: usize, value: i32) {
+fn insert_ith(arr: &mut [i32], i: usize, value: i32) {
 
-    // in this problem, we must insert i at a trivial location
-    // assume the extreme, and i index is 0
-    // inserting value there moves every item in arr to the left
-    // we also assume that len < capacity from allocator to make problem simpler
+    // we will be inserting at ith index, anywhere in the array
+    // lets do extreme, insert at 0th index
+    // inserting at 0th index shifts all other items to the right 1 index
+    // so index + 1 = index position val, since next index will be prev index val
+    // iterating backwards would make sense as well, since that clears up space for insertion at the end
 
-    // if we move items in arr to the right, we need to make space to replace first index item
-    // given we are solving the extreme
+    // we do not need check for len against capacity, just assume
+    for index in (i .. arr.len() - 1).rev() {
 
-    // iterate backwards
-    // last index = length - 1, then iterate backwards 1 by 1
-    // we want to iterate to i + 1, since i will simply be replaced
-    for index in (i + 1..length).rev() {
-
-        arr[index] = arr[index - 1]; // next index val == prev val
+        arr[index + 1] = arr[index];
 
     }
 
